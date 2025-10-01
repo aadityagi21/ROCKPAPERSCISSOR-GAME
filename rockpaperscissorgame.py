f8be2195668fec5c81969d27f5df438047d9ec6a
aadityagi21 <adityamcu1245@gmail.com>
2025-10-01 18:40:58 +0530

    Create rockpaperscissorgame.py

---
+++ rockpaperscissorgame.py
@@ -0,0 +1,19 @@
+print("*********************** ROCK , PAPER , SCISSOR GAME ******************")
+print("********** r STANDS FOR ROCK , p STANDS FOR PAPER , s STANDS FOR SCISSOR ***************")
+
+i=1
+while(i):
+    a=input("Enter your choice PLAYER 1 ==")
+    b=input("Enter your choice PLAYER 2 ==")
+    if a=='r' and b=='s': print("Congratulations Player 1 you Wins")
+    elif a=='s' and b=='r': print("Congratulations Player 2 you Wins")
+    elif a=='p' and b=='s': print("Congratulations Player 2 you Wins")
+    elif a=='s' and b=='p': print("Congratulations Player 1 you Wins")
+    elif a=='r' and b=='p': print("Congratulations Player 2 you Wins")
+    elif a=='p' and b=='r': print("Congratulations Player 1 you Wins")
+    elif a=='' and b=='': print("Pls Enter Something")
+    elif a=='': print("Why Are You Not Entering Your Choice Player 1")
+    elif b=='': print("Why Are You Not Entering Your Choice Player 2")
+    elif(a==b) : print("Match Draw")
+    else:print("Pls Enter Valid Input")
+    i+=1
